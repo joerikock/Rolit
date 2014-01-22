2d54451ccbe3e67b72c03d7be9578ed5c40e7e43
joerikock <joerikock@live.nl>
2014-01-22 13:10:14 +0100

    d

--- src/MenuItems/Button.java
+++ src/MenuItems/Button.java
@@ -11,8 +11,22 @@ import com.me.mygdxgame.RollIt;
 
 public class Button extends GuiObject {
 
+	// Instance variables -------------------------------------------------------
+	
+	/**
+	 * The text to be displayed on the button.
+	 */
 	private String text;
 
+	/**
+	 * Create a button.
+	 * 
+	 * @param text
+	 * 			the text to be displayed on the button.
+	 * @param x
+	 * 			
+	 * @param y
+	 */
 	public Button(String text,float x, float y) {
 		super(text);
 		this.text = text;
